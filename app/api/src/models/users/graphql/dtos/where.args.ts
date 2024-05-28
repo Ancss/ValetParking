48dcda9816baa90cf1e3ee80dc05b6b2b3950718
Ancss <61501274+Ancss@users.noreply.github.com>
2024-05-28 16:37:23 +0800

    create users curd by nestjs-prisma-codegen

---
+++ app/api/src/models/users/graphql/dtos/where.args.ts
@@ -0,0 +1,78 @@
+import { Field, InputType, PartialType } from '@nestjs/graphql';
+import { Prisma } from '@prisma/client';
+import { RestrictProperties } from 'src/common/dtos/common.input';
+
+@InputType()
+export class UserWhereUniqueInput {
+  uid: string;
+}
+
+@InputType()
+export class UserWhereInputStrict
+  implements
+    RestrictProperties<
+      UserWhereInputStrict,
+      Omit<Prisma.UserWhereInput, 'Credentials' | 'AuthProvider' | 'Admin'>
+    >
+{
+  uid: string | Prisma.StringFilter<'User'>;
+  createdAt: string | Date | Prisma.DateTimeFilter<'User'>;
+  updatedAt: string | Date | Prisma.DateTimeFilter<'User'>;
+  name: string | Prisma.StringNullableFilter<'User'>;
+  image: string | Prisma.StringNullableFilter<'User'>;
+  Manager:
+    | (Prisma.Without<
+        Prisma.ManagerNullableRelationFilter,
+        Prisma.ManagerWhereInput
+      > &
+        Prisma.ManagerWhereInput)
+    | (Prisma.Without<
+        Prisma.ManagerWhereInput,
+        Prisma.ManagerNullableRelationFilter
+      > &
+        Prisma.ManagerNullableRelationFilter);
+  Valet:
+    | (Prisma.Without<
+        Prisma.ValetNullableRelationFilter,
+        Prisma.ValetWhereInput
+      > &
+        Prisma.ValetWhereInput)
+    | (Prisma.Without<
+        Prisma.ValetWhereInput,
+        Prisma.ValetNullableRelationFilter
+      > &
+        Prisma.ValetNullableRelationFilter);
+  Customer:
+    | (Prisma.Without<
+        Prisma.CustomerNullableRelationFilter,
+        Prisma.CustomerWhereInput
+      > &
+        Prisma.CustomerWhereInput)
+    | (Prisma.Without<
+        Prisma.CustomerWhereInput,
+        Prisma.CustomerNullableRelationFilter
+      > &
+        Prisma.CustomerNullableRelationFilter);
+  // Todo: Add the below field decorator only to the $Enums types.
+  // @Field(() => $Enums.x)
+
+  AND: UserWhereInput[];
+  OR: UserWhereInput[];
+  NOT: UserWhereInput[];
+}
+
+@InputType()
+export class UserWhereInput extends PartialType(UserWhereInputStrict) {}
+
+@InputType()
+export class UserListRelationFilter {
+  every?: UserWhereInput;
+  some?: UserWhereInput;
+  none?: UserWhereInput;
+}
+
+@InputType()
+export class UserRelationFilter {
+  is?: UserWhereInput;
+  isNot?: UserWhereInput;
+}
---
+++ app/api/src/models/users/graphql/entity/user.entity.ts
@@ -0,0 +1,16 @@
+import { Field, ObjectType } from '@nestjs/graphql';
+import { User as UserType } from '@prisma/client';
+import { RestrictProperties } from 'src/common/dtos/common.input';
+
+@ObjectType()
+export class User implements RestrictProperties<User, UserType> {
+  // Todo Add below to make optional fields optional.
+  // @Field({ nullable: true })
+  uid: string;
+  createdAt: Date;
+  updatedAt: Date;
+  @Field({ nullable: true })
+  name: string;
+  @Field({ nullable: true })
+  image: string;
+}
