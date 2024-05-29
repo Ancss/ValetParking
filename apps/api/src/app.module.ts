34a1fbbd86ba5ff71f0ee8c5f41c548d7ef5a629
Ancss <61501274+Ancss@users.noreply.github.com>
2024-05-29 11:24:04 +0800

    add module in module.ts

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -5,8 +5,22 @@ import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { join } from 'path';
 import { ConfigModule } from '@nestjs/config';
-import { UsersModule } from './models/users/users.module';
 import { JwtModule } from '@nestjs/jwt';
+import { UsersModule } from './models/users/users.module';
+import { AddressesModule } from './models/addresses/addresses.module';
+import { AdminsModule } from './models/admins/admins.module';
+import { BookingTimelinesModule } from './models/booking-timelines/booking-timelines.module';
+import { BookingsModule } from './models/bookings/bookings.module';
+import { CompaniesModule } from './models/companies/companies.module';
+import { CustomersModule } from './models/customers/customers.module';
+import { GaragesModule } from './models/garages/garages.module';
+import { ManagersModule } from './models/managers/managers.module';
+import { ReviewsModule } from './models/reviews/reviews.module';
+import { SlotsModule } from './models/slots/slots.module';
+import { StripeModule } from './models/stripe/stripe.module';
+import { ValetAssignmentsModule } from './models/valet-assignments/valet-assignments.module';
+import { ValetsModule } from './models/valets/valets.module';
+import { VerificationsModule } from './models/verifications/verifications.module';
 
 const MAX_AGE = 24 * 60 * 60;
 
@@ -27,7 +41,22 @@ const MAX_AGE = 24 * 60 * 60;
         numberScalarMode: 'integer',
       },
     }),
+    StripeModule,
+
     UsersModule,
+    AdminsModule,
+    CustomersModule,
+    ManagersModule,
+    ValetsModule,
+    CompaniesModule,
+    GaragesModule,
+    AddressesModule,
+    SlotsModule,
+    BookingsModule,
+    ValetAssignmentsModule,
+    BookingTimelinesModule,
+    ReviewsModule,
+    VerificationsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
